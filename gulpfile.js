69848591b8600750963977ceea37f699bb1d1fe0
Andreas Blaesius <andi@unlegacy-android.org>
2022-08-15 20:55:45 +0200

    Inital

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./assets/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/scss/*.scss', gulp.series('sass'));
+});
